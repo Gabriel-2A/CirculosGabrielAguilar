fc7d89b10b25a0faab66df6eafab8a748cf44346
Gabriel-2A <gaa.021205@gmail.com>
2022-03-03 11:47:45 -0600

    creacion de la clase circulo y main

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Circulo {
+import static java.lang.Math.PI;
+
+public class Circulo {
+
+    private float radio;
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float calcularArea(float radio){
+        float area = (float)((2*PI)*radio);
+        return area;
+    }
+
+    public float calcularDiametro(){
+
+    }
+
+    public float calcularCircunferencia(){
+
+    }
 }
