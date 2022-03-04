baa66c40196eb7b7cefc0179724aa16cd6856721
Gabriel-2A <gaa.021205@gmail.com>
2022-03-04 11:37:43 -0600

    correccion de errores en la clase main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
 
     public static void main(String[] args){
+        Circulo nada = new Circulo();
         Circulo cr = new Circulo(3.5f);
 
-        cr.calcularArea();
-        cr.calcularCircunferencia();
-        cr.calcularDiametro();
+        System.out.println(cr.calcularArea());
+        System.out.println(cr.calcularDiametro());
+        System.out.println(cr.calcularCircunferencia());
+
     }
 }
